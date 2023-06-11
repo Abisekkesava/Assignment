3b7b8b8e20dd8ca83d8489a90f0edf84ffb8c755
Abisekkesava <136216291+Abisekkesava@users.noreply.github.com>
2023-06-11 17:41:34 -0500

    Delete Assignment1.sql

--- Assignment1.sql
+++
@@ -1,3 +0,0 @@
-select e.empid,e.emp_date, datediff(month, a.min_date, e.emp_date) as datedif FROM [TestDB].[dbo].[empid_date] e
-inner join (SELECT [empid], min([emp_date]) as min_date
-FROM [TestDB].[dbo].[empid_date] group by empid) a on a.empid = e.empid
